06ecdb1c20a33ec4131f59820a2ed1f1444772df
zhanna0611 <56571801+zhanna0611@users.noreply.github.com>
2019-12-14 13:37:46 +0600

    Add files via upload

---
+++ microservices 2/src/test/java/carrent/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package carrent.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
